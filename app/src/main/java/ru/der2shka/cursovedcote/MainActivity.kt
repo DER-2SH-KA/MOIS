7e2f103135e9e7ca547537724cb9ba2fa5590bea
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-01-31 15:09:24 +0700

    first commit

---
+++ app/src/main/java/ru/der2shka/cursovedcote/MainActivity.kt
@@ -0,0 +1,139 @@
+package ru.der2shka.cursovedcote
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.der2shka.cursovedcote.ui.theme.CursovedCotETheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CursovedCotETheme {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier
+                            .background(Color.White)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("ResourceAsColor")
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val height = 50.dp
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(R.color.background_color)
+            )
+        ,
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .height(height)
+                .background(
+                    color = colorResource(R.color.primary_blue)
+                )
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .height(height)
+                .background(
+                    color = colorResource(R.color.secondary_cyan)
+                )
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .height(height)
+                .background(
+                    color = colorResource(R.color.tertiary_orange)
+                )
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .height(height)
+                .background(
+                    color = colorResource(R.color.successful_green)
+                )
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .height(height)
+                .background(
+                    color = colorResource(R.color.error_red)
+                )
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .height(height)
+                .background(
+                    color = colorResource(R.color.warning_yellow)
+                )
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .height(height)
+                .background(
+                    color = colorResource(R.color.main_text_dark_gray)
+                )
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .height(height)
+                .background(
+                    color = colorResource(R.color.secondary_text_gray)
+                )
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .height(height)
+                .background(
+                    color = colorResource(R.color.additional_purple)
+                )
+        )
+
+        Text(
+            text = isSystemInDarkTheme().toString(),
+            color = Color.White
+        )
+    }
+}
---
+++ app/src/main/java/ru/der2shka/cursovedcote/ui/theme/Color.kt
@@ -0,0 +1,37 @@
+package ru.der2shka.cursovedcote.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// val VeryLightBlue = Color()
+
+// Light Theme.
+val SoftBlue = Color(0x4990e2)
+val SoftCyan = Color(0x4fe3c1)
+val VividOrange = Color(0xf5a623)
+val StrongGreen = Color(0x7ed320)
+val StrongRed = Color(0xd0011b)
+val VividYellow = Color(0xf8e71d)
+val VeryDarkGray = Color(0x333333)
+val VeryDarkGrayLighter = Color(0x666666)
+val VividViolet = Color(0x8f13fd)
+val VeryLightGrayMostlyWhite = Color(0xf9f9f9)
+
+// Dark Theme.
+val VeryLightBlue = Color(0x6ba9ff)
+val SoftCyanLighter = Color(0x70fee4)
+val LightOrange = Color(0xffc04e)
+val SoftGreen = Color(0x9aeb5e)
+val VividRed = Color(0xff3b2f)
+val LightYellow = Color(0xffeb3c)
+val VeryLightGray = Color(0xe0e0e0)
+val DarkGray = Color(0xa0a0a0)
+val VeryLightViolet = Color(0xab6bff)
+val VeryDarkGrayMostlyBlack = Color(0x1e1e1e)
