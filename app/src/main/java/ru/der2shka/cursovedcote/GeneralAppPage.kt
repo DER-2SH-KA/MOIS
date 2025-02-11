1ea0b77259fe2b1ac5c69438159e836eff247caf
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-02-11 13:55:31 +0700

    Added days of weekend and monthes in strings.xml

---
+++ app/src/main/java/ru/der2shka/cursovedcote/GeneralAppPage.kt
@@ -0,0 +1,24 @@
+package ru.der2shka.cursovedcote
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun GenetalAppPage(
+    navController: NavHostController
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.9f)
+        ,
+        contentAlignment = Alignment.Center
+    ) {
+    }
+}
