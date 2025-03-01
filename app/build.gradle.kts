e6495d09536973f7ed34e9e8416c884526043a77
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-03-01 23:07:32 +0700

    Add kapt into build.gradle.kts (app)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-
+    alias(libs.plugins.kotlin.android) apply true
+    kotlin("kapt")
 }
 
 android {
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     testImplementation(libs.androidx.room.testing)
+    kapt(libs.androidx.room.compiler)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
